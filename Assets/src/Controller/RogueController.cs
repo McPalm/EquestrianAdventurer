bd31bdbb01d948f8ebae53cb513b140d1ae39f3e
Palm <tauman.exe@gmail.com>
2017-03-20 17:56:13 +0100

    Initial Commit

---
+++ Assets/src/Controller/RogueController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Mobile))]
+public class RogueController : MonoBehaviour
+{
+
+
+
+	float inputcooldown = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(inputcooldown > 0f)
+		{
+			inputcooldown -= Time.deltaTime;
+			return;
+		}
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		if (x < 0f) Move(new Vector2(-1f, 0f));
+		else if (x > 0f) Move(new Vector2(1f, 0f));
+		else if (y < 0f) Move(new Vector2(0f, -1f));
+		else if (y > 0f) Move(new Vector2(0f, 1f));
+	}
+
+	void Move(Vector2 where)
+	{
+		print(where);
+		GetComponent<Mobile>().MoveTo((Vector2)transform.position + where);
+		EndTurn();
+	}
+
+	void EndTurn()
+	{
+		inputcooldown = 0.19f;
+	}
+}
---
+++ Assets/src/Map/Mobile.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mobile : MonoBehaviour
+{
+	// Vector2 NetworkedPosition = new Vector2(999999999f, 99999999999f);
+
+	public void MoveTo(Vector2 v2)
+	{
+		//NetworkedPosition = new Vector2(Mathf.Round(v2.x), Mathf.Round(v2.y));
+		OnMove(v2);
+	}
+
+	void OnMove(Vector2 v2)
+	{
+		StopAllCoroutines();
+		float distance = ((Vector2)transform.position - v2).magnitude;
+		StartCoroutine(Tween(transform.position, v2, distance/10f + 0.1f));
+		// transform.position = v2;
+	}
+
+	IEnumerator Tween(Vector2 start, Vector2 destination, float duration)
+	{
+		if (duration > 0.4f) duration = 0.4f; // prevent slow moves
+		float time = 0f;
+		while(time < duration)
+		{
+			time += Time.deltaTime;
+			transform.position = Vector2.Lerp(start, destination, time / duration);
+			yield return new WaitForSeconds(0f);
+		}
+		transform.position = destination;
+	}
+}
+
