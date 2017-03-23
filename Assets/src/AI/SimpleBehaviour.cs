81247f90f97f2eaefeb2a308e1380a9ca60af64c
Palm <tauman.exe@gmail.com>
2017-03-23 12:42:04 +0100

    A simple AI that tries to move straight at the player

---
+++ Assets/src/AI/SimpleBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// give it a target and it will move to the target and bump it.
+/// </summary>
+[RequireComponent(typeof(MapCharacter))]
+public class SimpleBehaviour : MonoBehaviour
+{
+	public GameObject target;
+
+	/// <summary>
+	/// called when the AI is given an action
+	/// </summary>
+	public void DoTurn()
+	{
+		Vector2 delta = target.transform.position - transform.position;
+
+		if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+		{
+			if (delta.x < 0f) MoveDirection(Vector2.left);
+			else if (delta.x > 0f) MoveDirection(Vector2.right);
+		}
+		else if(delta.y != 0f)
+		{
+			if (delta.y < 0f) MoveDirection(Vector2.down);
+			else if (delta.y > 0f) MoveDirection(Vector2.up);
+		}
+	}
+
+	void MoveDirection(Vector2 v2)
+	{
+		MapCharacter mc = null;
+		bool moved = GetComponent<Mobile>().MoveDirection(v2, out mc);
+		if (mc)
+			GetComponent<MapCharacter>().Melee(mc);
+		if(!moved)
+		{
+			print("obstacle!");
+		}
+	}
+}
---
+++ Assets/src/Gameplay/TurnTracker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TurnTracker : MonoBehaviour
+{
+
+	List<SimpleBehaviour> characters;
+
+	// Use this for initialization
+	void Start () {
+		characters = new List<SimpleBehaviour>(FindObjectsOfType<SimpleBehaviour>());
+	}
+
+	float timer = 1f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		timer -= Time.deltaTime;
+
+		if (timer < 0f)
+		{
+			foreach (SimpleBehaviour sb in characters)
+				sb.DoTurn();
+			timer = 1f;
+		}
+	}
+}
