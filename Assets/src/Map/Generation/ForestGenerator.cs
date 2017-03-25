d4ea3814ec4353e9069cbedf5c2dc15ebf3ef4e0
Palm <tauman.exe@gmail.com>
2017-03-25 16:19:46 +0100

    Added minimum path generator onto ForestGenerator

--- Assets/src/Map/Generation/ForestGenerator.cs
+++ Assets/src/Map/Generation/ForestGenerator.cs
@@ -22,20 +22,23 @@ public class ForestGenerator : IGenerator
 		generator.iterations = 2;
 		generator.Generate(connections);
 		AddResults(generator.GetResult());
-		generator.Generate(connections);
+		generator.Generate(0);
 		AddResults(generator.GetResult());
-		generator.density = 0.44f;
+		generator.density = 0.45f;
 		generator.iterations = 5;
 		generator.Generate(0);
 		AddResults(generator.GetResult());
 
-		// generate between adjacent sections
+		// Union with a minimum path o make sure we got connection to other areas
+		MinimumPath minPath = new MinimumPath();
+		minPath.Generate(connections);
+		UnionResults(minPath.GetResult());
 
 		// scatter a few walls around that will be turned into trees and rocks in the next step
 		// they will only be placed in such spots that they do not block any potential paths.
-		for (int x = 0; x < MapSectionData.DIMENSIONS; x++)
+		for (int x = 1; x < MapSectionData.DIMENSIONS - 1; x++)
 		{
-			for (int y = 0; y < MapSectionData.DIMENSIONS; y++)
+			for (int y = 1; y < MapSectionData.DIMENSIONS - 1; y++)
 			{
 				if (UnityEngine.Random.value < foilageDensity)
 				{
@@ -43,15 +46,15 @@ public class ForestGenerator : IGenerator
 					{
 						int nearbywalls = 0;
 						// cardinals
-						if (x > 0 && map[x - 1][y] == 0) nearbywalls++;
-						if (x < MapSectionData.DIMENSIONS - 1 && map[x + 1][y] == 0) nearbywalls++;
-						if (y > 0 && map[x][y - 1] == 0) nearbywalls++;
-						if (y < MapSectionData.DIMENSIONS - 1 && map[x][y + 1] == 0) nearbywalls++;
+						if (map[x - 1][y] == 0) nearbywalls++;
+						if (map[x + 1][y] == 0) nearbywalls++;
+						if (map[x][y - 1] == 0) nearbywalls++;
+						if (map[x][y + 1] == 0) nearbywalls++;
 						// diagonals
-						if (x > 0 && y > 0 && map[x - 1][y - 1] == 0) nearbywalls++;
-						if (x > 0 && y < MapSectionData.DIMENSIONS - 1 && map[x - 1][y + 1] == 0) nearbywalls++;
-						if (x < MapSectionData.DIMENSIONS - 1 && y > 0 && map[x + 1][y - 1] == 0) nearbywalls++;
-						if (x < MapSectionData.DIMENSIONS - 1 && y < MapSectionData.DIMENSIONS - 1 && map[x + 1][y + 1] == 0) nearbywalls++;
+						if (map[x - 1][y - 1] == 0) nearbywalls++;
+						if (map[x - 1][y + 1] == 0) nearbywalls++;
+						if (map[x + 1][y - 1] == 0) nearbywalls++;
+						if (y < MapSectionData.DIMENSIONS - 1 && map[x + 1][y + 1] == 0) nearbywalls++;
 
 						if (nearbywalls == 0)
 							map[x][y] = 0;
