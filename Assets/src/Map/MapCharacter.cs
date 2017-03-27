d65796e5d777251d11b6103f305f2ab18eb79dea
Palm <tauman.exe@gmail.com>
2017-03-27 21:43:21 +0200

    Show player Stats in Inventory

--- Assets/src/Map/MapCharacter.cs
+++ Assets/src/Map/MapCharacter.cs
@@ -16,6 +16,8 @@ public class MapCharacter : MonoBehaviour
 	public GameObject skull;
 
 	public UnityEvent EventDeath = new UnityEvent();
+	public MapCharacterEvent EventUpdateStats = new MapCharacterEvent();
+	public MapCharacterEvent EventKillingBlow = new MapCharacterEvent();
 
 	// Use this for initialization
 	void Start ()
@@ -35,10 +37,11 @@ public class MapCharacter : MonoBehaviour
 		bool hit = Mathf.Min(Random.value, Random.value) < (hitSkill / (target.dodgeSkill + hitSkill));
 		if (hit)
 		{
-			float damage = (0.75f + Random.value * 0.5f) * baseDamage * (5f + baseDamage/2f) / (5f + baseDamage/2f + target.armor);
+			float damage = (0.75f + Random.value * 0.5f) * baseDamage * 10f / (10f + target.armor);
 			if (damage < 1f) damage = 1;
 			target.GetComponent<HitPoints>().Hurt(new DamageData().SetDamage((int)damage));
 			HurtPool.Instance.DoHurt(target.GetComponent<MapObject>().RealLocation, (int)damage);
+			if (target.GetComponent<HitPoints>().CurrentHealth <= 0) EventKillingBlow.Invoke(target);
 		}
 		else
 			CombatTextPool.Instance.PrintAt((Vector3)target.GetComponent<MapObject>().RealLocation + new Vector3(0f, 0.4f), "Dodge", Color.cyan);
@@ -75,5 +78,9 @@ public class MapCharacter : MonoBehaviour
 		}
 
 		GetComponent<HitPoints>().MaxHealth = hp;
+
+		EventUpdateStats.Invoke(this);
 	}
+
+	public class MapCharacterEvent : UnityEvent<MapCharacter> { }
 }
---
+++ Assets/src/UI/Character/StatSummary.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StatSummary : MonoBehaviour
+{
+	public Text text;
+	public MapCharacter character;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		CalcStats(character);
+		character.EventUpdateStats.AddListener(CalcStats);
+	}
+	
+	
+	void CalcStats (MapCharacter character)
+	{
+		text.text = "Max HP: " + character.GetComponent<HitPoints>().MaxHealth
+		 + "\nAttack: " + character.baseDamage
+		 + "\nHit: " + character.hitSkill
+		 + "\nArmor: " + character.armor + " (" + Mathf.Round(100f * (1f - 10f / (10f + character.armor))) + "%)"
+		 + "\nDodge: " + character.dodgeSkill;
+	}
+}
