3907ea8761d6d8b67cfb38932b667cbe2cf8aacb
Palm <tauman.exe@gmail.com>
2017-03-30 10:34:17 +0200

    Added a pause for the DragFollow. Camera should now wait a for seconds before it follows the player during a respawn

--- Assets/src/SceneGizmos/DragFollow.cs
+++ Assets/src/SceneGizmos/DragFollow.cs
@@ -6,10 +6,17 @@ public class DragFollow : MonoBehaviour
 	public GameObject target;
 	public float distance = 2f;
 	public float smoothing = 0.1f;
+	public float pause = 0f;
+
 
 	// Update is called once per frame
 	void LateUpdate ()
 	{
+		if(pause > 0f)
+		{
+			pause -= Time.deltaTime;
+			return;
+		}
 		if(!target)
 		{
 			enabled = false;
@@ -18,5 +25,10 @@ public class DragFollow : MonoBehaviour
 		Vector3 delta = target.transform.position - transform.position;
 		if(delta.x < -distance || delta.x > distance || delta.y < -distance || delta.y > distance)
 			transform.position += delta * smoothing;
+	}
+
+	public void Pause(float seconds)
+	{
+		pause = seconds;
 	}
 }
