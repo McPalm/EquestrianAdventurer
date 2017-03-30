dceaad0af0b7c068635596add71c2d32f25a6303
Palm <tauman.exe@gmail.com>
2017-03-30 09:22:31 +0200

    Player teleport back to the beginning and is healed to full on death

---
+++ Assets/src/Map/Gizmos/Teleport.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour
+{
+	/// <summary>
+	/// teleports the given object to the target location
+	/// </summary>
+	/// <param name="c"></param>
+	/// <param name="targetLocation"></param>
+	public void TeleportObject(Component c, GameObject targetLocation)
+	{
+		MapObject o = c.GetComponent<MapObject>();
+		if(o)
+		{
+			o.Put(targetLocation.transform.position);
+		}
+	}
+
+	/// <summary>
+	/// Teleports the component to the location of this object
+	/// </summary>
+	/// <param name="c"></param>
+	public void TeleportHere(Component c)
+	{
+		TeleportObject(c, gameObject);
+	}
+}
