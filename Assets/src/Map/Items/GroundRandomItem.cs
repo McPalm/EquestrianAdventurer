a3cc2ebc544b116e904ff3eeb023a5d7c9d35ac7
Palm <tauman.exe@gmail.com>
2017-03-30 13:17:30 +0200

    GroundRandomItem, it randomized the item out of a set of items. Its a better and more specialized way of doing a roll table for items.

---
+++ Assets/src/Map/Items/GroundRandomItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundRandomItem : GroundItem
+{
+	public GroundItem[] common;
+	public GroundItem[] uncommon;
+	public GroundItem[] rare;
+
+	public override Item CloneItem()
+	{
+		if(rare.Length > 0 && Random.value < 0.02f)
+		{
+			return rare[Random.Range(0, rare.Length)].CloneItem();
+		}
+		else if(uncommon.Length > 0 && Random.value < 0.2f)
+		{
+			return uncommon[Random.Range(0, uncommon.Length)].CloneItem();
+		}
+		else
+		{
+			return common[Random.Range(0, common.Length)].CloneItem();
+		}
+	}
+}
