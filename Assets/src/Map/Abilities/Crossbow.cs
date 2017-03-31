40ee4354b3cb03a3c49770b358e39d7810dd9a4e
Palm <tauman.exe@gmail.com>
2017-03-31 13:44:16 +0200

    Added Ranged Attack, readded Crossbow as inhereting from RangedAttack

---
+++ Assets/src/Map/Abilities/Crossbow.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crossbow : RangedAttack
+{
+	public int ReloadTime = 4;
+	public int Damage = 7;
+
+
+	new protected void Start()
+	{
+		EventOnHit.AddListener(OnHit);
+		EventOnMiss.AddListener(OnMiss);
+		base.Start();
+	}
+
+	int reloadTimer = 0;
+
+	public override bool Useable
+	{
+		get
+		{
+			return Loaded;
+		}
+	}
+
+	public bool Loaded
+	{
+		get
+		{
+			return reloadTimer <= 0;
+		}
+	}
+
+	void OnHit(MapCharacter target)
+	{
+		float damage = (0.75f + Random.value * 0.5f) * Damage * 20f / (20f + target.armor);  // hardcoding armour penetration!
+		if (damage < 1f) damage = 1;
+		target.GetComponent<HitPoints>().Hurt(new DamageData().SetDamage((int)damage));
+		HurtPool.Instance.DoHurt(target.GetComponent<MapObject>().RealLocation, (int)damage);
+		reloadTimer = ReloadTime;
+	}
+
+	void OnMiss(MapCharacter target)
+	{
+		CombatTextPool.Instance.PrintAt((Vector3)target.GetComponent<MapObject>().RealLocation + new Vector3(0f, 0.4f), "Miss", Color.cyan);
+		reloadTimer = ReloadTime;
+	}
+
+	/// <summary>
+	/// Reload the crossbow
+	/// </summary>
+	/// <returns>True if the bow is ready to fire</returns>
+	public bool Reload()
+	{
+		if (Loaded) return true;
+		reloadTimer--;
+		float percentage = (float)(ReloadTime - reloadTimer) / (float)ReloadTime * 100f;
+		CombatTextPool.Instance.PrintAt((Vector3)me.RealLocation + new Vector3(0f, 0.4f), "Reloading " + Mathf.Round(percentage) + "%", Color.cyan);
+		return Loaded;
+	}
+}
---
+++ Assets/src/Map/Abilities/RangedAttack.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class RangedAttack : MonoBehaviour
+{
+	public int Range = 10;
+
+	protected MapCharacter user;
+	protected MapObject me;
+	protected LOSCheck LoS;
+
+	public MapCharacterEvent EventOnHit = new MapCharacterEvent();
+	public MapCharacterEvent EventOnMiss = new MapCharacterEvent();
+
+	// Use this for initialization
+	protected void Start ()
+	{
+		user = GetComponent<MapCharacter>();
+		me = GetComponent<MapObject>();
+		LoS = GetComponent<LOSCheck>();
+	}
+
+	virtual public bool CanTarget(MapCharacter target)
+	{
+		return LoS.HasLOE(target.GetComponent<MapObject>(), Range);
+	}
+
+	virtual public bool Useable
+	{
+		get
+		{
+			return true;
+		}
+	}
+
+	virtual protected bool Hit(MapCharacter target)
+	{
+		int range = IntVector2Utility.PFDistance(target.GetComponent<MapObject>().RealLocation, GetComponent<MapObject>().RealLocation);
+		return Random.value < (user.hitSkill / (user.hitSkill + ((range == 1) ? target.dodgeSkill : target.dodgeSkill / 2) + range));
+	}
+
+	public bool Attack(MapCharacter target)
+	{
+		if (!Useable) return false;
+		if (!CanTarget(target)) return false;
+		if (Hit(target)) EventOnHit.Invoke(target);
+		else EventOnMiss.Invoke(target);
+		return true;
+	}
+
+	[System.Serializable]
+	public class MapCharacterEvent : UnityEvent<MapCharacter> { }
+}
