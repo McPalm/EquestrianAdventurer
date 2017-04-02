9153f819f40a2e8b86369d7b5ca33e86194aa697
Palm <tauman.exe@gmail.com>
2017-04-02 16:19:00 +0200

    struct stats

---
+++ Assets/src/Gameplay/Stats.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Stats
+{
+	public int hp;
+	public int hit;
+	public int dodge;
+	public int armor;
+	public int damage;
+	public int armorpen;
+
+	public float BaseHitChance(Stats target)
+	{
+		return hit / (float)(hit + target.dodge);
+	}
+
+	public int DamageVersus(Stats target, float multiplier = 1f, float flatBonus = 0f)
+	{
+		return (int)((damage * target.DamageReduction(armorpen) * multiplier) + flatBonus);
+	}
+
+	public float DamageReduction(int armorpen)
+	{
+		if(armorpen < -8)
+		{
+			armorpen = -8;
+		}
+		return (10f + armorpen) / (10f + armorpen + armor);
+	}
+}
--- Assets/src/Sandbox/Heart.cs
+++ Assets/src/Sandbox/Heart.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Heart : MonoBehaviour {
-
+public class Heart : MonoBehaviour
+{
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Start ()
+	{
+		Stats a = new Stats();
+		Stats b = new Stats();
+
+		a.armor = 0;
+		print(a.DamageReduction(0));
+
+		a.armor = 0;
+		print(a.DamageReduction(10));
+
+		a.armor = 10;
+		print(a.DamageReduction(0));
+
+		a.armor = 10;
+		print(a.DamageReduction(10));
+
+		a.armor = 20;
+		print(a.DamageReduction(0));
+
+		a.armor = 20;
+		print(a.DamageReduction(10));
 	}
 }
