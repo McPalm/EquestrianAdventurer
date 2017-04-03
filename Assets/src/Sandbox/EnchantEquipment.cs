d497e8cc19a60e5004daa0134bc0d57e2671b22d
Palm <tauman.exe@gmail.com>
2017-04-03 13:21:13 +0200

    Added a number of extra mundane qualitites

--- Assets/src/Sandbox/EnchantEquipment.cs
+++ Assets/src/Sandbox/EnchantEquipment.cs
@@ -23,17 +23,12 @@ public static class EnchantEquipment
 				e.stats.hit -= 2;
 			}
 		}
-		else if (Random.value < 0.2f) // fine
+		else if (Random.value < 0.25f) // fine
 		{
-			e.displayName = "Fine " + e.displayName;
 			if (e.slots == EquipmentType.body)
-			{
-				e.stats.dodge += 1;
-			}
+				MundaneArmorQuality(e);
 			else
-			{
-				e.stats.hit += 1;
-			}
+				MundaneWeaponQuality(e);
 		}
 
 		if(Random.value < 0.07f) // magical
@@ -53,6 +48,74 @@ public static class EnchantEquipment
 				e.stats.hit += plus;
 				e.stats.damage += plus;
 			}
+		}
+	}
+
+	public static void MundaneWeaponQuality(Equipment e)
+	{
+		switch(Random.Range(0, 5))
+		{
+			case 1:
+				e.displayName = "Fine " + e.displayName;
+				e.stats.hit += 1;
+				break;
+			case 2:
+				e.displayName = "Hefty " + e.displayName;
+				e.stats.damage += 1;
+				e.stats.hit -= 1;
+				break;
+			case 3:
+				e.displayName = "Serrated " + e.displayName;
+				e.stats.armorpen -= 1;
+				e.stats.damage += 1;				
+				break;
+			case 4:
+				e.displayName = "Masterwork " + e.displayName;
+				e.stats.armorpen += 1;
+				break;
+		}
+	}
+
+	public static void MundaneArmorQuality(Equipment e)
+	{
+		switch (Random.Range(0, 5))
+		{
+			case 1:
+				e.displayName = "Fine " + e.displayName;
+				e.stats.dodge += 1;
+				break;
+			case 2:
+				if (e.stats.armor > 0)
+				{
+					e.displayName = "Reinforced " + e.displayName;
+					e.stats.dodge -= (1 + e.stats.armor / 5);
+					e.stats.armor += (1 + e.stats.armor / 5);
+				}
+				else
+				{
+					e.displayName = "Armored " + e.displayName;
+					e.stats.armor += (1 + e.stats.dodge / 5);
+					e.stats.dodge -= (1 + e.stats.dodge / 5);
+				}
+				break;
+			case 3:
+				if (e.stats.armor > 0)
+				{
+					e.displayName = "Spiked " + e.displayName;
+					e.stats.damage += (1 + e.stats.armor / 5);
+					e.stats.armor -= (1 + e.stats.armor / 5);
+				}
+				else
+				{
+					e.displayName = "Lovely " + e.displayName;
+					e.stats.dodge -= (1 + e.stats.dodge / 2);
+					e.stats.hit += (1 + e.stats.dodge / 2);
+				}
+				break;
+			case 4:
+				e.displayName = "Masterwork " + e.displayName;
+				e.stats.armor += 1;
+				break;
 		}
 	}
 }
