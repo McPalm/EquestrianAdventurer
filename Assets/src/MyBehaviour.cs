eb2f4439c9fce80b06b16c07cffaa02be93feff7
Palm <tauman.exe@gmail.com>
2017-04-07 10:52:45 +0200

    MyBehaviour, bad name actually. Uses events to deal with disabling and Enables to simplify subscribing and unsubscribing wihtout casuing teardown errors.

---
+++ Assets/src/MyBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+/// <summary>
+/// Safe teardown for things that need to subscribe/unsubscribe
+/// </summary>
+public class MyBehaviour : MonoBehaviour
+{
+	protected UnityEvent EventDisable = new UnityEvent();
+	protected UnityEvent EventDestroy = new UnityEvent();
+	protected UnityEvent EventQuit = new UnityEvent();
+
+	bool teardown = false;
+
+	protected void OnDisable()
+	{
+		if (teardown) return;
+		EventDisable.Invoke();
+	}
+
+	protected void OnDestroy()
+	{
+		if (teardown) return;
+		EventDestroy.Invoke();
+	}
+
+	protected void OnApplicationQuit()
+	{
+		teardown = true;
+		EventQuit.Invoke();
+	}
+}
