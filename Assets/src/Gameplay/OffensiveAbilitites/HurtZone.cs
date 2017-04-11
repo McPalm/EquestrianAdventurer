a309821e07ba64556c88c221bcb9f0f66f5e3666
Palm <tauman.exe@gmail.com>
2017-04-11 18:19:43 +0200

    An acid puddle that hurts whoever steps upon it

---
+++ Assets/src/Gameplay/OffensiveAbilitites/HurtZone.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class HurtZone : MyBehaviour, TurnTracker.TurnEntry
+{
+	public float damage;
+	public DamageTypes damageType;
+	public int duration;
+
+	public bool scale = true;
+	public float startSize;
+
+	public void DoTurn()
+	{
+		MapCharacter mc = ObjectMap.Instance.CharacterAt(GetComponent<MapObject>().RealLocation);
+		if(mc)
+		{
+			float currentDamage = (scale) ? damage * duration / startSize : damage;
+			DamageData d = new DamageData(gameObject)
+				.AddType(damageType)
+				.SetDamage(currentDamage * Random.Range(0.75f, 1.25f));
+			mc.GetComponent<HitPoints>().Hurt(d);
+		}
+		if (duration > 0)
+		{
+			duration--;
+			if (duration == 0) Destroy(gameObject);
+			else if(scale)
+			{
+				transform.localScale = Vector3.one * duration / startSize;
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		TurnTracker.Instance.Add(this);
+		EventDestroy.AddListener(MyDisable);
+		if(scale)
+		{
+			startSize = duration;
+		}
+	}
+
+	void MyDisable()
+	{
+		TurnTracker.Instance.Remove(this);
+	}
+}
