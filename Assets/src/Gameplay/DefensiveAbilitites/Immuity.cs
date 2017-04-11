32541b8377c27eab66a601a74e57783b8d3975a2
Palm <tauman.exe@gmail.com>
2017-04-11 18:24:10 +0200

    Immunity script. Become immune to A given type of damage.

---
+++ Assets/src/Gameplay/DefensiveAbilitites/Immuity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Immuity : MyBehaviour
+{
+	public DamageTypes immuneTo;
+
+	// Use this for initialization
+	void Start ()
+	{
+		EventDisable.AddListener(MyDisable);
+	}
+
+	void OnEnable()
+	{
+		GetComponent<HitPoints>().EventBeforeHurt.AddListener(OnHurt);
+	}
+
+	void OnHurt(DamageData d)
+	{
+		if (d.HasAnyType(immuneTo))
+			d.multiplier = 0f;
+	}
+
+	void MyDisable()
+	{
+		GetComponent<HitPoints>().EventBeforeHurt.RemoveListener(OnHurt);
+	}
+}
