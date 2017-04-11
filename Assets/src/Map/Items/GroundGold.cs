2ff202d529ac8bdcd33528b6dc5bd5352a147181
Palm <tauman.exe@gmail.com>
2017-04-11 13:03:55 +0200

    Limited Gold Drops

---
+++ Assets/src/Map/Items/GroundGold.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGold : GroundItem
+{
+
+	public int minVal = 1;
+	public int maxVal = 10;
+
+	public override Item CloneItem()
+	{
+		item.value = Random.Range(minVal, maxVal + 1);
+		item.category = ItemCategory.gold;
+		return base.CloneItem();
+	}
+}
