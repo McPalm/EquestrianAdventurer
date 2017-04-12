660706893492460a962fc39ac595d95ce29a7355
Palm <tauman.exe@gmail.com>
2017-04-12 12:10:32 +0200

    Sound Effects

---
+++ Assets/src/Sound/PlaySound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour
+{
+
+	public AudioClip[] clip;
+
+	public void Play()
+	{
+		SfxPool.Instance.Play(clip[Random.Range(0, clip.Length)], transform.position, 1f);
+	}
+
+	public void Play(float volume)
+	{
+		SfxPool.Instance.Play(clip[Random.Range(0, clip.Length)], transform.position, volume);
+	}
+}
---
+++ Assets/src/Sound/SfxPool.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SfxPool : MonoBehaviour
+{
+	static SfxPool _instance;
+
+	public AudioSource[] audioSources;
+
+	public static SfxPool Instance
+	{
+		get
+		{
+			return _instance;
+		}
+	}
+
+	int count = 0;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		_instance = this;
+	}
+	
+	public void Play(AudioClip audio, Vector2 location, float volume = 1f)
+	{
+		AudioSource a = audioSources[count];
+		count++;
+		count %= audioSources.Length;
+
+		a.transform.position = location;
+		a.clip = audio;
+		a.volume = volume;
+		a.Play();
+	}
+}
