6de684dcbd2c79e21da4ec53c882eddde6366c65
Palm <tauman.exe@gmail.com>
2017-04-14 13:31:39 +0200

    A lil popup that shows Gold Gains

---
+++ Assets/src/UI/Gizmos/AddNumberDisplay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AddNumberDisplay : MonoBehaviour {
+
+	public float TimeOut = 2f;
+	public string suffix;
+
+	public GameObject frame;
+	public Text text;
+
+	int currentSum;
+
+	public void Add(int i)
+	{
+		currentSum += i;
+		frame.SetActive(true);
+		if(currentSum > 0) text.text = "+" + currentSum.ToString() +  suffix;
+		else text.text = currentSum.ToString() + suffix;
+		StopAllCoroutines();
+		StartCoroutine(ResetIn(TimeOut));
+	}
+
+	IEnumerator ResetIn(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		Reset();
+	}
+
+	void Reset()
+	{
+		frame.SetActive(false);
+		currentSum = 0;
+	}
+}
