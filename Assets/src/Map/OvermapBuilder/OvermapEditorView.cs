c16ded5aa0d025d9cbe3cfbe01729b4d53f05c64
Palm <tauman.exe@gmail.com>
2017-04-25 16:33:12 +0200

    Select Sections and Groups for Editing.

--- Assets/src/Map/OvermapBuilder/OvermapEditorView.cs
+++ Assets/src/Map/OvermapBuilder/OvermapEditorView.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 public class OvermapEditorView : MonoBehaviour
 {
@@ -13,11 +14,14 @@ public class OvermapEditorView : MonoBehaviour
 
 	OvermapData model;
 	Dictionary<IntVector2, SectionMapIcon> tiles = new Dictionary<IntVector2, SectionMapIcon>();
+	System.Action editMode;
 
 	// Use this for initialization
 	void Start ()
 	{
 		// hack together a model for testing
+
+		editMode = SelectMode;
 
 		model = new OvermapData();
 
@@ -36,16 +40,48 @@ public class OvermapEditorView : MonoBehaviour
 
 		model.AddGroup(IntVector2.zero, new IntVector2(0, 1), new IntVector2(1, 1));
 
+		/*
 		groupView.Show(model, model.groups[0]);
 
 		sectionView.Show(model, new IntVector2(1, 1));
+		*/
+		SelectAt(new IntVector2(1, 1));
 	}
 	
-	// Update is called once per frame
-	void Update ()
+	void SelectAt(IntVector2 iv2)
 	{
-	
+		foreach(SectionMapIcon icon in tiles.Values)
+		{
+			icon.FocusTile = false;
+			icon.FocusGroup = false;
+		}
+
+		if(model.sections.ContainsKey(iv2))
+		{
+			// select thingy
+			sectionView.Show(model, iv2);
+			OvermapData.SectionGroupData group = model.GroupOf(iv2);
+			if (group != null)
+			{
+				foreach (IntVector2 member in group.members)
+				{
+					GetOrBuildAt(member).FocusGroup = true;
+				}
+				groupView.Show(model, group);
+			}
+			else
+				groupView.Hide();
+
+			GetOrBuildAt(iv2).FocusTile = true;
+		}
+		else
+		{
+			// deselect
+			groupView.Hide();
+			sectionView.Hide();
+		}
 	}
+
 
 	void RefreshView()
 	{
@@ -81,5 +117,23 @@ public class OvermapEditorView : MonoBehaviour
 		render = Instantiate(sectionIconPrefab, (Vector3)location, Quaternion.identity) as SectionMapIcon;
 		tiles.Add(location, render);
 		return render;
+	}
+
+
+
+	void Update()
+	{
+		editMode();
+	}
+
+	void SelectMode()
+	{
+		if (EventSystem.current.IsPointerOverGameObject()) return;
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			SelectAt(IntVector2.RoundFrom(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+			return;
+		}
 	}
 }
