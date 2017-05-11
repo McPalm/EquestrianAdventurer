c2c63c0d381678e1fce0a22172e1d1604b8e18f0
Palm <tauman.exe@gmail.com>
2017-05-11 13:25:29 +0200

    Keep track of Time

--- Assets/src/Gameplay/TimeAndDay.cs
+++ Assets/src/Gameplay/TimeAndDay.cs
@@ -2,9 +2,16 @@
 using UnityEngine.Events;
 using System.Collections;
 
-public class TimeAndDay : MonoBehaviour
+public class TimeAndDay : MonoBehaviour, TurnTracker.TurnEntry
 {
 	static TimeAndDay _instance;
+
+	int day = 1;
+	int hour = 10;
+	int second;
+
+	const int SecPerHour = 400;
+	const int HourPerDay = 24;
 
 	public static TimeAndDay Instance
 	{
@@ -16,9 +23,36 @@ public class TimeAndDay : MonoBehaviour
 	}
 
 	public UnityEvent EventNewDay = new UnityEvent();
+	public TimeEvent EventNewHour = new TimeEvent();
+
+	void Start()
+	{
+		TurnTracker.Instance.Add(this);
+	}
 
 	public void NewDay()
 	{
+		day++;
+		hour = 10;
+		second = 0;
 		EventNewDay.Invoke();
 	}
+
+	public void DoTurn()
+	{
+		second++;
+		if(second == SecPerHour)
+		{
+			second = 0;
+			hour++;
+			if(hour == HourPerDay)
+			{				
+				NewDay();
+			}
+			EventNewHour.Invoke(hour);
+		}
+	}
+
+	[System.Serializable]
+	public class TimeEvent : UnityEvent<int> { }
 }
